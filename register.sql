63d092e23680e21f5d7f3fcb863a3147380cd3fc
Kanishkaa R <kanishkaa1101@gmail.com>
2026-09-03 10:47:46 +0530

    Delete register.sql

--- register.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE gymdb; 
-USE gymdb; CREATE TABLE members (    
-id INT AUTO_INCREMENT PRIMARY KEY,     
-  name VARCHAR(100),     
-  email VARCHAR(100),     
-  plan VARCHAR(50),     
-  trainer VARCHAR(50) 
-  ); 
