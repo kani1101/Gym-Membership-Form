8ea756d43bf47ee8222ef25231c51da8f6394664
Kanishkaa R <kanishkaa1101@gmail.com>
2026-09-03 10:25:50 +0530

    Create register.sql

---
+++ register.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE gymdb; 
+USE gymdb; CREATE TABLE members (    
+id INT AUTO_INCREMENT PRIMARY KEY,     
+  name VARCHAR(100),     
+  email VARCHAR(100),     
+  plan VARCHAR(50),     
+  trainer VARCHAR(50) 
+  ); 
